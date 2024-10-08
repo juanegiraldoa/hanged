07eb731168b0aa7057e1e4e2bc5747a492bc37b2
Juan Giraldo <juanegiraldoa@proton.me>
2024-10-08 00:27:05 -0500

    Change ./ to @/

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import { render } from "preact";
 import { useEffect } from "preact/hooks";
-import { lettersArray } from "./constants/Letters";
-import useWord from "./hooks/useWord";
-import Typography from "./components/atoms/Typography";
-import Errors from "./components/templates/Errors";
-import Board from "./components/templates/Board";
-import Button from "./components/atoms/Button";
-import "./index.css";
+import { lettersArray } from "@/constants/Letters";
+import useWord from "@/hooks/useWord";
+import Typography from "@/components/atoms/Typography";
+import Errors from "@/components/templates/Errors";
+import Board from "@/components/templates/Board";
+import Button from "@/components/atoms/Button";
+import "@/index.css";
 
 function App() {
   const { selectLetter, letters, censoredWord, errors, resetGame } = useWord();
